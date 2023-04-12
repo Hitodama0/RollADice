2412a55d0db6e291a74dc3f021d8db5cedadb4d5
Biagio <biagior00@gmail.com>
2023-04-12 18:54:47 +0200

    Initial Commit

---
+++ RollADice/RollADiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollADiceApp.swift
+//  RollADice
+//
+//  Created by Biagio Ricci on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RollADiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
